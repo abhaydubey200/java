daa2fee5fb5865047378f1c4e5360d97722b24f3
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-21 01:52:26 +0530

    first

---
+++ turnuryop.java
@@ -0,0 +1,12 @@
+public class turnuryop {
+    public static void main(String[] args) {
+        
+    
+   int  num1 = 10;
+   int  num2 = 20;
+    
+   int res=(num1>num2) ? (num1+num2):(num1-num2);
+    System.out.println(res);
+   
+}
+}
