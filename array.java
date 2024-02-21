8b8315931464b3707041e9e4ef08f0f830b5f42d
Abhay <146931800+abhaydubey200@users.noreply.github.com>
2024-02-21 11:52:10 +0530

    main

---
+++ array.java
@@ -0,0 +1,39 @@
+/* 
+public class array {
+    public static void main(String[] args) {
+    
+    int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+myNumbers[1][2] = 9;
+System.out.println(myNumbers[1][2]);
+}
+    
+}
+
+
+public class array {
+    public static void main(String[] args) {
+      int[][] myNumbers = { {1, 2, 3, 4}, {5, 6, 7} };
+      for (int i = 0; i < myNumbers.length; ++i) {
+        for(int j = 0; j < myNumbers[i].length; ++j) {
+          System.out.println(myNumbers[i][j]);
+        }
+      }
+    }
+ }
+
+
+
+ public class array {
+ 
+    public static void main(String[] args) {
+        int [] []myNumbers={{1,2,3,4,5,6},{9,8,7,6,5,4,3,2}};
+        System.out.println(myNumbers.length);
+        for(int i=0;i<=myNumbers.length;++i){
+            for(int j=0;j<=myNumbers.length;++j){
+                System.out.println(myNumbers[i][j]);
+                
+            }
+        }
+    }
+ }
+  */
